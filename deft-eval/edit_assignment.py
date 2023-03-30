e67fe7d76637df76ed174cfa23c6f17ba726ee22
bartek.jezierski <bartek.jezierski@glanos.de>
2023-03-30 18:55:49 +0200

    Update button

--- deft-eval/edit_assignment.py
+++ deft-eval/edit_assignment.py
@@ -34,13 +34,13 @@ def colorize_text(sentence, tags_sequence_pred):
         word = words[i]
         tag = tags[i]
         if tag == "O":
-            colored_word = f'<button onclick="set_tag(this, \'Term\')">{word}</button>'
+            colored_word = f'{word}'
             colored_words.append(colored_word)
         elif "Term" in tag:
-            colored_word = f'<button onclick="set_tag(this, \'Definition\')"><span style="color:red">{word}</span></button>'
+            colored_word = f'<span style="color:red">{word}</span>'
             colored_words.append(colored_word)
         elif "Definition" in tag:
-            colored_word = f'<button onclick="set_tag(this, \'O\')"><span style="color:blue">{word}</span></button>'
+            colored_word = f'<span style="color:blue">{word}</span>'
             colored_words.append(colored_word)
     colored_sentence = " ".join(colored_words)
     return colored_sentence
@@ -63,30 +63,6 @@ def main():
         # Create a copy of the DataFrame to store the user's changes
         df_edited = df.copy()
 
-        # Define the JavaScript function to set the tag of a button
-        set_tag_js = """
-        <script>
-        function set_tag(button, tag) {
-            var sentence_index = button.getAttribute('data-sentence-index');
-            var word_index = button.getAttribute('data-word-index');
-            var tags_input = document.getElementById('tags-' + sentence_index);
-            var tags = tags_input.value.split(' ');
-            tags[word_index] = tag;
-            tags_input.value = tags.join(' ');
-            button.innerHTML = button.innerHTML.replace(/<[^>]*>/g, '');
-            if (tag === 'Term') {
-                button.innerHTML = '<span style="color:red">' + button.innerHTML + '</span>';
-                button.setAttribute('onclick', 'set_tag(this, \'Definition\')');
-            } else if (tag === 'Definition') {
-                button.innerHTML = '<span style="color:yellow">' + button.innerHTML + '</span>';
-                button.setAttribute('onclick', 'set_tag(this, \'O\')');
-            } else {
-                button.setAttribute('onclick', 'set_tag(this, \'Term\')');
-            }
-        }
-        </script>
-        """
-
         # Display the sentences with their tags
         st.write("## Sentences")
         for i in range(len(df)):
@@ -97,7 +73,19 @@ def main():
             tags = df_edited.iloc[i]["tags_sequence_pred"].split()
             tags_input = st.empty()
             tags_input.text_input("Tags", " ".join(tags), key=f"tags-{i}")
-            st.markdown(set_tag_js, unsafe_allow_html=True)
+
+            # Add a button to update the coloring based on the edited tags
+            if st.button(f"Update colors for sentence {i+1}"):
+                new_tags = tags_input.text_input(
+                    "Tags", " ".join(tags), key=f"new-tags-{i}")
+                df_edited.at[i, "tags_sequence_pred"] = new_tags
+                if len(new_tags.split()) != len(df_edited.iloc[i]["tokens"].split()):
+                    st.markdown("Should be same length",
+                                unsafe_allow_html=True)
+                else:
+                    colored_sentence = colorize_text(
+                        df_edited.iloc[i]["tokens"], new_tags)
+                    st.markdown(colored_sentence, unsafe_allow_html=True)
 
         # Allow the user to download the edited file
         if st.button("Download edited file"):
