4505dfe87484186288e8793779a12e59c62b338e
bartek.jezierski <bartek.jezierski@glanos.de>
2023-04-21 09:44:27 +0200

    Delete sentences on the go, adjust the execution flow

--- deft-eval/edit_assignment.py
+++ deft-eval/edit_assignment.py
@@ -1,7 +1,7 @@
-import spacy
 import streamlit as st
 import pandas as pd
 import base64
+import numpy as np
 
 # Set up the logger
 import logging
@@ -11,6 +11,11 @@ logger.setLevel(logging.DEBUG)
 
 # Create a StreamHandler to display logs in Streamlit
 # streamlit run edit_assignment.py
+
+
+# Instantiate the Session State Variables
+if 'cache' not in st.session_state:
+    st.session_state.cache = {'start': 0, 'end': 0}
 
 
 class StreamHandler(logging.Handler):
@@ -38,7 +43,10 @@ def colorize_text(sentence, tags_sequence_pred):
         if tag == "O":
             colored_word = f'{word}'
             colored_words.append(colored_word)
-        elif "Term" in tag:
+        elif "Alias-Term" in tag:
+            colored_word = f'<span style="color:pink">{word}</span>'
+            colored_words.append(colored_word)
+        elif "Term" in tag and "Alias-Term" not in tag:
             colored_word = f'<span style="color:red">{word}</span>'
             colored_words.append(colored_word)
         elif "Definition" in tag:
@@ -57,7 +65,7 @@ def simplify_tags(tags):
     return " ".join(updated_tags)
 
 
-@st.experimental_memo
+# @st.experimental_memo
 def prepend_tags(tags, tokens):
     tokens = tokens.split()
     if tags == "-":
@@ -80,6 +88,46 @@ def prepend_tags(tags, tokens):
                 updated_tags.append("B-Definition")
             else:
                 updated_tags.append("I-Definition")
+        elif "alias-term" in tag or "at" == tag:
+            if "Alias-Term" not in prev_tag:
+                updated_tags.append("B-Alias-Term")
+            else:
+                updated_tags.append("I-Alias-Term")
+        elif "ordered-term" in tag or "ot" == tag:
+            if "Ordered-Term" not in prev_tag:
+                updated_tags.append("B-Ordered-Term")
+            else:
+                updated_tags.append("I-Ordered-Term")
+        elif "referential-term" in tag or "rt" == tag:
+            if "Referential-Term" not in prev_tag:
+                updated_tags.append("B-Referential-Term")
+            else:
+                updated_tags.append("I-Referential-Term")
+        elif "definition" in tag or "d" == tag:
+            if "Definition" not in prev_tag:
+                updated_tags.append("B-Definition")
+            else:
+                updated_tags.append("I-Definition")
+        elif "secondary definition" in tag or "sd" == tag:
+            if "Secondary Definition" not in prev_tag:
+                updated_tags.append("B-Secondary Definition")
+            else:
+                updated_tags.append("I-Secondary Definition")
+        elif "ordered definition" in tag or "od" == tag:
+            if "Ordered Definition" not in prev_tag:
+                updated_tags.append("B-Ordered Definition")
+            else:
+                updated_tags.append("I-Ordered Definition")
+        elif "referential definition" in tag or "rd" == tag:
+            if "Referential Definition" not in prev_tag:
+                updated_tags.append("B-Referential Definition")
+            else:
+                updated_tags.append("I-Referential Definition")
+        elif "qualifier" in tag or "q" == tag:
+            if "Qualifier" not in prev_tag:
+                updated_tags.append("B-Qualifier")
+            else:
+                updated_tags.append("I-Qualifier")
         elif "o" == tag:
             updated_tags.append("O")
         else:
@@ -107,30 +155,51 @@ def update_sent_type(tags, sent_type, sent_type_score):
 def sort(df):
     df = df.sort_values(
         by=["sent_type_pred", "sent_type_scores"], ascending=[False, True])
-    df = df.reset_index(drop=True)
     return df
+
+
+def form_callback(df_edited, i, tokens):
+    tags_input_value = st.session_state[f"tags-{i}"]
+    tags_input_value = prepend_tags(tags_input_value, tokens)
+    df_edited.at[i, "tags_sequence_pred"] = tags_input_value
+    st.session_state.cache["df"] = df_edited
+
+
+def upload_file_callback():
+    st.experimental_memo.clear()
 
 
 def main():
     st.set_page_config(page_title="Tag Editor", page_icon=":pencil2:")
 
     # Get the file from the user
-    uploaded_file = st.file_uploader("Choose a TSV file", type=["tsv"])
+    uploaded_file = st.file_uploader("Choose a TSV file", type=[
+                                     "tsv"], on_change=upload_file_callback)
 
     if uploaded_file is not None:
         # Read the file into a DataFrame
-        df = pd.read_csv(uploaded_file, sep="\t")
-        df = sort(df)
+        if "df" not in st.session_state.cache:
+            df = pd.read_csv(uploaded_file, sep="\t")
+            df = sort(df)
+            df = df.drop_duplicates(subset='tokens', keep="first")
+            df = df.reset_index(drop=True)
+            for index, row in df.iterrows():
+                if isinstance(row['tokens'], float):
+                    df.at[index, 'tokens'] = np.nan
+            df = df.dropna()
+            # Create a copy of the DataFrame to store the user's changes
+            df_edited = df.copy()
+            st.session_state.cache["df"] = df_edited
+        else:
+            df_edited = st.session_state.cache["df"]
+            df_edited = sort(df_edited)
 
-        df["tags_sequence_pred"] = df.apply(
+        df_edited["tags_sequence_pred"] = df_edited.apply(
             lambda row: simplify_tags(row["tags_sequence_pred"]), axis=1)
 
         # Colorize the sentences based on their tags
-        df["colored_tokens"] = df.apply(lambda row: colorize_text(
+        df_edited["colored_tokens"] = df_edited.apply(lambda row: colorize_text(
             row["tokens"], row["tags_sequence_pred"]), axis=1)
-
-        # Create a copy of the DataFrame to store the user's changes
-        df_edited = df.copy()
 
         # Set up pagination
         page_size = 20
@@ -138,8 +207,14 @@ def main():
         current_page = st.session_state.get("current_page", 1)
 
         st.write("## Sentences")
-        start = max((current_page - 1) * page_size, 0)
-        end = min(current_page * page_size, len(df_edited))
+
+        def update_page():
+            col2.write(f"Page {current_page}/{page_count}")
+            st.session_state.cache["start"] = max(
+                (current_page - 1) * page_size, 0)
+            st.session_state.cache["end"] = min(
+                current_page * page_size, len(df_edited))
+            st.session_state.cache["df"] = df_edited
 
         # Display the current page number
         if st.button("Update"):
@@ -147,49 +222,42 @@ def main():
 
         # Add navigation buttons
         col1, col2, col3 = st.columns(3)
-        if col1.button("Prev") and current_page > 1:
+        if col1.button("Prev", key="prev1") and current_page > 1:
             current_page -= 1
-        col2.write(f"Page {current_page}/{page_count}")
-        if col3.button("Next") and current_page < page_count - 1:
+            update_page()
+            if col3.button("Next", key="next1") and current_page < page_count - 1:
+                current_page += 1
+                update_page()
+        elif col3.button("Next", key="next1") and current_page < page_count - 1:
             current_page += 1
+            update_page()
+        else:
+            update_page()
         st.session_state["current_page"] = current_page
 
-        # with container.beta_expander(f"Sentences (Page {page}/{page_count})"):
-        for i in range(start, end):
-            sentence = df.iloc[i]["colored_tokens"]
-            st.markdown(sentence, unsafe_allow_html=True)
-            st.markdown("Pred: "+str(df.iloc[i]["sent_type_pred"])+", score:"+str(df.iloc[i]
-                                                                                  ["sent_type_scores"]), unsafe_allow_html=True)
+        to_refresh = False
+        for i in range(st.session_state.cache["start"], st.session_state.cache["end"]):
+            sentence = df_edited.iloc[i]["colored_tokens"]
+            st.markdown(f"<br />No. {i} <br />{sentence}",
+                        unsafe_allow_html=True)
+            st.markdown("Pred: "+str(df_edited.iloc[i]["sent_type_pred"])+", score:"+str(df_edited.iloc[i]
+                                                                                         ["sent_type_scores"]), unsafe_allow_html=True)
 
             tags = df_edited.iloc[i]["tags_sequence_pred"].split()
             tokens = df_edited.iloc[i]["tokens"]
             tags_input = st.empty()
 
-            if st.button(f"Label full definition", key=f"d-{i}"):
-                tags_input_value = tags_input.text_input(
-                    "Tags", value="d "*len(tags), key=f"tags-{i}")
-            elif st.button(f"Clear", key=f"d-{i}"):
-                tags_input_value = tags_input.text_input(
-                    "Tags", value="O "*len(tags), key=f"tags-{i}")
-            else:
-                tags_input_value = tags_input.text_input(
-                    "Tags", value=" ".join(
-                        tags), key=f"tags-{i}")
+            tags_input.text_input(
+                "Tags", value=" ".join(
+                    tags), key=f"tags-{i}", on_change=form_callback, args=(df_edited, i, tokens))
 
-            # Add a button to update the coloring based on the edited tags
-            with st.expander(f"Update colors for sentence {i}"):
-                tags_input_value = prepend_tags(tags_input_value, tokens)
-                df_edited.at[i, "tags_sequence_pred"] = tags_input_value
-                # if len(tags_input_value.split()) != len(df_edited.iloc[i]["tokens"].split()):
-                #     st.markdown(f"Should be same length len1 {len(tags_input_value.split())} len2 {len(df_edited.iloc[i]['tokens'].split())}",
-                #                 unsafe_allow_html=True)
-                # else:
-                print("tags_input_value", tags_input_value)
-                colored_sentence = colorize_text(
-                    df_edited.iloc[i]["tokens"], tags_input_value)
-                st.markdown(
-                    "Pred: "+str(df_edited.iloc[i]["sent_type_pred"]))
-                st.markdown(colored_sentence, unsafe_allow_html=True)
+            if st.button("Delete", key=f"delete-{i}"):
+                df_edited_new = df_edited.drop(i)
+                df_edited_new.reset_index(drop=True, inplace=True)
+                st.session_state.cache["df"] = df_edited_new
+                to_refresh = True
+            if to_refresh and i == st.session_state.cache["end"]-1:
+                st.experimental_rerun()
 
         # Allow the user to download the edited file
         if st.button("Download edited file"):
@@ -197,6 +265,7 @@ def main():
                 lambda row: prepend_tags(row["tags_sequence_pred"], row["tokens"]), axis=1)
             df_edited["sent_type_pred"], df_edited["sent_type_scores"] = zip(*df_edited.apply(
                 lambda row: update_sent_type(row["tags_sequence_pred"], row["sent_type_pred"], row["sent_type_scores"]), axis=1))
+            st.session_state.cache["df"] = df_edited
             csv = df_edited.to_csv(index=False, sep="\t")
             b64 = base64.b64encode(csv.encode()).decode()
             href = f'<a href="data:file/csv;base64,{b64}" download="edited_file.tsv">Download edited file</a>'
